3c2b6f550a4af596041c38cbb26c158152768928
luyuan <1101873740@qq.com>
2018-05-17 17:28:01 +0800

    [update] 扩展方法 report md5 memory sdFile 等

--- src/main/java/com/march/wxcube/common/Extensions.kt
+++ src/main/java/com/march/wxcube/common/Extensions.kt
@@ -1,9 +1,18 @@
 package com.march.wxcube.common
 
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Environment
+import android.view.View
 import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONArray
 import com.alibaba.fastjson.JSONObject
 import com.march.wxcube.Weex
+import java.io.File
+import java.lang.Exception
+import java.math.BigInteger
+import java.nio.charset.Charset
+import java.security.MessageDigest
 
 /**
  * CreateAt : 2018/4/18
@@ -29,6 +38,32 @@ fun <T> JSONArray?.toList(clz: Class<T>): List<T> {
     return JSONArray.parseArray(json, clz)
 }
 
-fun Any.report(msg: String) {
-    Weex.getInst().mWeexInjector.onErrorReport(null, msg)
+// 提交错误
+fun Any.report(msg: String, throwable: Throwable? = null) {
+    if (Weex.getInst().mWeexConfig.debug) {
+        Weex.getInst().mWeexInjector.onErrorReport(throwable, msg)
+    }
+}
+
+fun Context.memory(float: Float): Int {
+    val activityManager = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    return (activityManager.memoryClass * 1024 * 1024 * float).toInt()
+}
+
+fun Any.sdFile(): File {
+    return Environment.getExternalStorageDirectory();
+}
+
+fun String.md5(): String {
+    return try {
+        val md = MessageDigest.getInstance("MD5")
+        val md5Data = BigInteger(1, md.digest(toByteArray(Charset.forName("utf-8"))))
+        String.format("%032X", md5Data)
+    } catch (e: Exception) {
+        ""
+    }
+}
+
+fun View.click(f: (View) -> Unit) {
+    setOnClickListener { f(this) }
 }
