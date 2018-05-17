d2a341fcf647130dca5b2f612ef22fa17dbde361
luyuan <1101873740@qq.com>
2018-05-17 17:24:00 +0800

    [update] 在 Index 页面中进行数据初始化

--- src/main/java/com/march/wxcube/ui/IndexActivity.kt
+++ src/main/java/com/march/wxcube/ui/IndexActivity.kt
@@ -1,9 +1,10 @@
 package com.march.wxcube.ui
 
-import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.march.common.utils.LogUtils
 import com.march.wxcube.Weex
-import com.taobao.weex.common.Constants.Name.Recycler.TYPE_INDEX
 
 
 /**
@@ -12,17 +13,33 @@ import com.taobao.weex.common.Constants.Name.Recycler.TYPE_INDEX
  *
  * @author chendong
  */
-class IndexActivity : WxBaseActivity() {
+class IndexActivity : BaseActivity() {
+
+    companion object {
+        const val TIME_START = 1500L
+    }
+
+    private val mCtx by lazy { IndexActivity@ this }
+    private val mIndexRunnable = object : Runnable {
+        override fun run() {
+            if (!Weex.getInst().mWeexRouter.openIndexPage(mCtx)) {
+                LogUtils.e("检测一次，没有准备好")
+                mHandler.postDelayed(this, 50)
+            } else {
+                mHandler.removeCallbacksAndMessages(null)
+                finish()
+                overridePendingTransition(0, 0)
+            }
+        }
+    }
+
+    val mHandler by lazy { Handler(Looper.getMainLooper()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Weex.getInst().mWeexInjector.onPageCreated(this,Weex.PAGE_INDEX)
-        Weex.getInst().mWeexUpdater.requestPages(true, this)
-    }
-
-    override fun startActivity(intent: Intent?) {
-        super.startActivity(intent)
-        finish()
-        overridePendingTransition(0, 0)
+        Weex.getInst().mWeexInjector.onPageCreated(this, Weex.PAGE_INDEX)
+        Weex.getInst().mWeexInjector.getLoadingHandler().setIndexPageContent(this)
+        // 2s 后启动
+        mHandler.postDelayed(mIndexRunnable, TIME_START)
     }
 }
