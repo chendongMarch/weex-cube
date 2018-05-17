d4bcdd6f21c28fe23b45528045a8e595f59a7527
luyuan <1101873740@qq.com>
2018-05-17 17:28:21 +0800

    [add] 分离 Render 类

---
+++ src/main/java/com/march/wxcube/ui/WeexRender.kt
@@ -0,0 +1,45 @@
+package com.march.wxcube.ui
+
+import android.app.Activity
+import com.march.common.utils.LogUtils
+import com.march.wxcube.Weex
+import com.march.wxcube.common.report
+import com.march.wxcube.model.WeexPage
+import com.taobao.weex.IWXRenderListener
+import com.taobao.weex.RenderContainer
+import com.taobao.weex.WXSDKInstance
+import com.taobao.weex.common.WXRenderStrategy
+
+/**
+ * CreateAt : 2018/5/3
+ * Describe :
+ *
+ * @author chendong
+ */
+class WeexRender(activity: Activity,
+                 private val mWxInst: WXSDKInstance,
+                 private val listener: IWXRenderListener) {
+
+    init {
+        val renderContainer = RenderContainer(activity)
+        mWxInst.setRenderContainer(renderContainer)
+        mWxInst.registerRenderListener(listener)
+    }
+
+    fun render(page: WeexPage, opts: Map<String, Any>, consumer: ((String?) -> Unit)? = null) {
+        if (!page.isValid) {
+            listener.onException(mWxInst, "100", "页面数据有问题")
+            return
+        }
+        Weex.getInst().mWeexJsLoader.getTemplateAsync(mWxInst.context, page) {
+            LogUtils.e("render thread = ${Thread.currentThread().name}")
+            if (!it.isNullOrBlank()) {
+                consumer?.invoke(it)
+                mWxInst.render(page.pageName, it, opts, null, WXRenderStrategy.APPEND_ASYNC)
+            } else {
+                report("render error " + page.toString())
+                listener.onException(mWxInst, "101", "js 没有准备好")
+            }
+        }
+    }
+}
