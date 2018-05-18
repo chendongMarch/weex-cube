dbb6210fc3f0fc9a548d5f9357e47850a78bcdee
luyuan <1101873740@qq.com>
2018-05-18 10:07:16 +0800

    [update] 数据结构调整，删除无用字段

--- src/main/java/com/march/wxcube/model/WeexPage.kt
+++ src/main/java/com/march/wxcube/model/WeexPage.kt
@@ -7,75 +7,52 @@ import android.text.TextUtils
 
 /**
  * CreateAt : 2018/3/26
- * Describe :
+ * Describe : 页面资源数据结构
  *
  * @author chendong
  */
 class WeexPage() : Parcelable {
 
-    var _comment: String? = null
+    var _comment: String? = null // 对该页面的注释，生产环境不返回
 
-    var pageName: String? = null
-
-    var remoteJs: String? = null // 远程 js
-
-    var localJs: String? = null // 本地 js
-        get() {
-            if (field == null) {
-
-                field = "$pageName-$jsVersion-js".replace(".", "-")
-            }
-            return field
-        }
-
-    var assetsJs: String? = null // assets js
-
-    var appVersion: String? = null // app 最小支持版本
+    var pageName: String? = null // 页面名称
 
     var jsVersion: String? = null // weex 构建版本
 
-    var jsMd5: String? = null // js 的 md5 值，用来标记文件有没有修改
+    var remoteJs: String? = null // 远程 js
 
-    var webUrl: String? = null  // 降级 web，也是唯一标记
+    var appVersion: String? = null // app 最小支持版本
 
-    var indexPage: Boolean = false
+    var webUrl: String? = null  // 降级 web，也是页面的唯一标记
 
-    val isValid: Boolean
+    var indexPage: Boolean = false // 该页面上是否被标记为首页
+
+    /*********************⬆️以上是数据结构字段⬇️下面是临时生成字段************************/
+
+    val key: String?
+        get() = "$pageName($jsVersion)".replace(".", "")
+
+    val localJs: String? // 本地 js 文件名
+        get() = "$pageName-$jsVersion-js".replace(".", "-")
+
+    val assetsJs: String? // assets js 文件名
+        get() = "js/$pageName-$jsVersion-js".replace(".", "-")
+
+    val isValid: Boolean //
         get() = (!TextUtils.isEmpty(pageName)
                 && !TextUtils.isEmpty(jsVersion)
                 && !TextUtils.isEmpty(webUrl)
-                && isAnyJsValid)
-
-    private val isAnyJsValid: Boolean
-        get() = (!TextUtils.isEmpty(remoteJs)
-                || !TextUtils.isEmpty(assetsJs)
-                || !TextUtils.isEmpty(localJs))
+                && !TextUtils.isEmpty(remoteJs))
 
     fun make(openUrl: String): WeexPage {
         val page = WeexPage()
         page.pageName = this.pageName
         page.remoteJs = this.remoteJs
-        page.localJs = this.localJs
-        page.assetsJs = this.assetsJs
         page.appVersion = this.appVersion
         page.jsVersion = this.jsVersion
-        page.jsMd5 = this.jsMd5
         page.webUrl = openUrl
         return page
     }
-
-
-    override fun hashCode(): Int {
-        return 43
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is WeexPage) {
-            return false
-        }
-        return pageName == other.pageName && jsVersion == other.jsVersion
-    }
-
 
     override fun toString(): String {
         return "WeexPage{" +
@@ -85,7 +62,6 @@ class WeexPage() : Parcelable {
                 ", assetsJs='" + assetsJs + '\'' +
                 ", appVersion='" + appVersion + '\'' +
                 ", jsVersion='" + jsVersion + '\'' +
-                ", jsMd5='" + jsMd5 + '\'' +
                 ", webUrl='" + webUrl + '\'' +
                 '}'
     }
@@ -97,18 +73,14 @@ class WeexPage() : Parcelable {
         dest.writeString(this.assetsJs)
         dest.writeString(this.appVersion)
         dest.writeString(this.jsVersion)
-        dest.writeString(this.jsMd5)
         dest.writeString(this.webUrl)
     }
 
     constructor(`in`: Parcel) : this() {
         this.pageName = `in`.readString()
         this.remoteJs = `in`.readString()
-        this.localJs = `in`.readString()
-        this.assetsJs = `in`.readString()
         this.appVersion = `in`.readString()
         this.jsVersion = `in`.readString()
-        this.jsMd5 = `in`.readString()
         this.webUrl = `in`.readString()
     }
 
