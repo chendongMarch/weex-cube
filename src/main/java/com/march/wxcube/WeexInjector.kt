01861ba097cf3ae28cd368b211baa1c1fc0a0d3c
luyuan <1101873740@qq.com>
2018-05-22 16:43:48 +0800

    [update] 丰富注入器的方法

--- src/main/java/com/march/wxcube/WeexInjector.kt
+++ src/main/java/com/march/wxcube/WeexInjector.kt
@@ -1,7 +1,7 @@
 package com.march.wxcube
 
 import android.app.Activity
-import android.util.Log
+import com.march.common.utils.LogUtils
 import com.march.wxcube.loading.ILoadingHandler
 import com.march.wxcube.loading.LoadingHandlerImpl
 import com.taobao.weex.InitConfig
@@ -15,22 +15,48 @@ import okhttp3.OkHttpClient
  */
 interface WeexInjector {
 
+    /**
+     * 错误打印
+     */
     fun onErrorReport(throwable: Throwable?, errorMsg: String) {
-
+        LogUtils.e("wx-error", errorMsg)
+        throwable?.let { LogUtils.e("wx-error", it) }
     }
 
+    /**
+     * log 打印
+     */
     fun onLog(tag: String, msg: String) {
-        Log.e(tag, msg)
+        LogUtils.e(tag, msg)
     }
 
-    fun onInitWeex(builder: InitConfig.Builder) {}
+    /**
+     * 初始化 WxSdkEngine
+     */
+    fun onWxSdkEngineInit(builder: InitConfig.Builder) {}
 
+    /**
+     * 注册 module,component
+     */
+    fun onWxModuleCompRegister() {}
+
+    /**
+     * 内置 Activity 创建时调用
+     */
     fun onPageCreated(activity: Activity, type: Int) {
 
     }
 
+    /**
+     * 网络请求初始化，自动移
+     */
     fun onInitOkHttpClient(builder: OkHttpClient.Builder) {}
 
+    /**
+     * loading
+     * 容器渲染时支持蒙版
+     * 使用内置首页的自定义界面
+     */
     fun getLoadingHandler(): ILoadingHandler {
         return LoadingHandlerImpl()
     }
