13151f634b6e8dfe08f1f34083adf55a3d2fdbb5
luyuan <1101873740@qq.com>
2018-06-05 21:16:59 +0800

    [add] 增加 performer 机制，独立功能可以绑定到生命周期上

---
+++ src/main/java/com/march/wxcube/performer/IPerformer.kt
@@ -0,0 +1,13 @@
+package com.march.wxcube.performer
+
+import com.march.wxcube.lifecycle.WeexLifeCycle
+
+/**
+ * CreateAt : 2018/6/5
+ * Describe : 作为 weex 页面的一部分，独立的完成一个功能
+ *
+ * @author chendong
+ */
+interface IPerformer : WeexLifeCycle {
+
+}
